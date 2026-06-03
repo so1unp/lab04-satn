a62081e492c56733446ee20af7977d48a239ff40
JoaqQ1 <quirogajuaco7@gmail.com>
2026-06-03 14:32:04 -0300

    Modificacion de estacion para estandarizar nomenclatura

--- estacion.c
+++ estacion.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
+#ifndef STATION_H
+#define STATION_H
+
 #include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
 
 #define MAX_HANGAR_CAPACITY 3
-#define MAX_FUEL_CAPACITY 500
 
-/**
- * La estacion tiene su posisionamiento en x e y,
- * su nombre, la capacidad maxima de naves, la cantidad de naves
- * que contiene hasta el momento
- *
- */
 typedef struct
 {
     char name[30];
@@ -31,42 +25,6 @@ typedef struct
     bool fuelWarningSent;  // true si ya notificó a las naves
     bool isDestroyed;      // true si se quedó sin combustible y explotó
 
-} Estacion;
+} station;
 
-void mostrarEstacion(Estacion aEstacion);
-
-int main(int argc, char *argv[])
-{
-    // Agregar código aquí.
-
-    Estacion estacion;
-    strcpy(estacion.name, "Estacion 1");
-    estacion.pos_x = 10;
-    estacion.pos_y = 20;
-    estacion.hangarCapacity = MAX_HANGAR_CAPACITY;
-    estacion.currentShips = 0;
-    estacion.fuelCapacity = MAX_FUEL_CAPACITY;
-    estacion.currentFuel = 0;
-    estacion.isDestroyed = false;
-
-    mostrarEstacion(estacion);
-    
-    // Termina la ejecución del programa.
-    exit(EXIT_SUCCESS);
-}
-
-void mostrarEstacion(Estacion aEstacion)
-{
-    printf("Nombre: %s\n", aEstacion.name);
-    printf("Posicion en x: %d\n", aEstacion.pos_x);
-    printf("Posicion en y: %d\n", aEstacion.pos_y);
-    printf("Capacidad de naves: %d\n", aEstacion.hangarCapacity);
-    printf("Cantidad de naves: %d\n", aEstacion.currentShips);
-    printf("Capacidad de combustible: %d\n", aEstacion.fuelCapacity);
-    printf("Cantidad de combustible: %d\n", aEstacion.currentFuel);
-    if(!aEstacion.isDestroyed){
-        printf("Estado de la estacion: HABILITADA\n");
-    } else {
-        printf("Estado de la estacion: INHABILITADA\n");
-    }
-}
+#endif
